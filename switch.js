9416720678e397a9469e361c5b6f0720d566c0f3
teena231 <Teenaprajapath@gmail.com>
2026-05-04 14:28:14 +0530

    fibonnaciseries

---
+++ switch.js
@@ -0,0 +1,16 @@
+let n1=13;
+let n2=2;
+let operator="*";
+switch(operator){
+    case "+":
+    console.log(n1+n2);
+    break;
+  case "-":
+    console.log(n1-n2);
+    break;
+  case "*":
+    console.log(n1*n2);
+    break;
+  default:
+    console.log("Invalid operator");
+}
